1b3124d8acf0872c634ed5edcf76c382b8582e72
ruslankobrin <ruslankobrin@gmail.com>
2020-07-10 00:55:00 +0300

    set env.sh settings

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
-SECRET_KEY = '3728edbfd5b0cd45b09202fc2cf'
-SECRET_KEY_PAYMENT = 'SecretKey01'
-SHOP_ID = '5'
-SECRET_KEY_CSRF = bytes("8a57170b245bd2901352286a30d54a86".encode('utf-8'))
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY_PAYMENT = os.environ.get('SECRET_KEY_PAYMENT')
+SHOP_ID = os.environ.get('SHOP_ID')
+SECRET_KEY_CSRF = bytes(os.environ.get('SECRET_KEY_CSRF').encode('utf-8'))
 PAYWAY = 'payeer_rub'
