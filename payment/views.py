6f77656af1f5cc3755dee30b306da86a38ad67dc
ruslankobrin <ruslankobrin@gmail.com>
2020-07-09 19:57:24 +0300

    refactor form.py code

--- payment/views.py
+++ payment/views.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template, request, session
 
-from payment.forms import RegistrationForm
+from payment.forms import PaymentForm
 
 payment = Blueprint(name='payment', import_name=__name__, template_folder='templates')
 
 @payment.route('/', methods=('GET', 'POST'))
 def index():
-    form = RegistrationForm(request.form, meta={'csrf_context': session})
+    form = PaymentForm(request.form, meta={'csrf_context': session})
     if request.method == 'POST' and form.validate():
         print(form.amount.data)
         print(form.currency.data)
